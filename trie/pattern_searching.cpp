95dbba58bc2211fabd5141ab996f0ce9fbf2309e
Chandan Dwivedi <chhandan.dwivedi99@gmail.com>
2017-07-04 07:27:28 +0530

    update

--- trie/pattern_searching.cpp
+++ trie/pattern_searching.cpp
@@ -1,3 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define CHAR_TO_INDEX(str) (int)str-(int)'a'
+struct trienode{
+    bool isleaf;
+    int index[1000];
+    trienode* children[50];
+};
