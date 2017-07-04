4a541e9262814d07d70df3e6baa65750e27fc41b
Chandan Dwivedi <chhandan.dwivedi99@gmail.com>
2017-07-04 07:32:08 +0530

    update

--- trie/pattern_searching.cpp
+++ trie/pattern_searching.cpp
@@ -6,3 +6,14 @@ struct trienode{
     int index[1000];
     trienode* children[50];
 };
+trienode* getnewnode(void){
+    trienode* new_node=NULL;
+    new_node=new trienode;
+    if(new_node){
+        new_node->isleaf=false;
+        for(int i=0;i<26;i++){
+            new_node->children[i]=NULL;
+        }
+    }
+    return new_node;
+}
